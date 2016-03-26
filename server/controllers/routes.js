96a9ab1d9c231f11c8b593d74e9afe26da80953b
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-03-26 16:56:49 -0700

    added initial blog page

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,5 +1,7 @@
 var index = require('./index');
+var blog = require('./index');
 
 module.exports = {
-  index: index
+  index: index,
+  blog: blog
 }
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 // set the routes path
 app.use('/api', router.index);
-
+app.use('/blog', router.blog);
 // always redirect to angular app for routes
 // are not registered
 app.get('/', function(req,res){
